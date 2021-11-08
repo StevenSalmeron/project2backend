d8a140087082426ce7ace5289c78579b7709080d
StevenSalmeron <92335046+StevenSalmeron@users.noreply.github.com>
2021-11-08 00:26:52 -0800

    Delete Incomplete/project-2 directory

--- Incomplete/project-2/src/main/java/com/revature/project2/model/ERole.java
+++
@@ -1,5 +0,0 @@
-package com.revature.project2.model;
-
-public enum ERole {
-	ROLE_USER, ROLE_MODERATOR, ROLE_ADMIN
-}
--- Incomplete/project-2/src/test/java/com/revature/project2/servicetests/TicketServiceImplTest.java
+++
@@ -1,5 +0,0 @@
-package com.revature.project2.servicetests;
-
-public class TicketServiceImplTest {
-
-}
